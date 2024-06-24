85363a648c348d5207ae577bcbff165effe00a3b
Wiston <wthyago-2000@hotmail.com>
2024-06-24 10:30:11 -0300

    Adição de um callback para evitar repetição de código, com a busca por id.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: %i[show edit]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -22,7 +24,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
@@ -39,6 +40,10 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
