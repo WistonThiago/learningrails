04b77ac8d930e6b8e92db50b110b9aec47d4b889
Wiston <wthyago-2000@hotmail.com>
2024-06-30 00:49:33 -0300

    Finalizada a inst. do Bootstrap e teste com modais e tooltips.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
